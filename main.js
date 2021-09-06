82b74f99324850dd35bbf82f8c66d309dc9110c1
chucho49 <53028273+chucho49@users.noreply.github.com>
2021-09-05 22:48:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function capturaDeDatos(){
      var ho_ex_di_dominical=document.getElementById("heddominicales").value;
      var ho_noc_dominical=document.getElementById("hndominicales").value;
      var Ho_ex_noc_dominical=document.getElementById("hendominicales").value;
+     var AuxTrans=53227;
         
         /* Horas Diurnas*/
         var ho_diurna_total= hora*ho_diurna;
@@ -55,9 +56,18 @@ function capturaDeDatos(){
 
         /* */
 
-        var valor_total=ho_diurna_total+ho_ex_diurna_total2+ho_noc_total+ho_ex_noc_total2+ho_di_dominical_total2+ho_ex_di_dominical_total2+ho_noc_dominical_total2+Ho_ex_noc_dominical_total2;
+        var valor_total=ho_diurna_total+ho_ex_diurna_total2+ho_noc_total+ho_ex_noc_total2+ho_di_dominical_total2+ho_ex_di_dominical_total2+ho_noc_dominical_total2+Ho_ex_noc_dominical_total2+AuxTrans;
+
+        /* salud y pension */
+
+        var salud= valor_total*0.036;
+        var pension= valor_total*0.036;
+        var deducidos=salud+pension;
+
+        var valor_total2=valor_total-deducidos;
 
         /* */
+        
 
 
 
@@ -74,12 +84,16 @@ function capturaDeDatos(){
         document.getElementById("ho_ex_di_dominical_text").innerText=` $ `+ parseInt(ho_ex_di_dominical_total2);
         document.getElementById("ho_noc_dominical_text").innerText=` $ `+ parseInt(ho_noc_dominical_total2);
         document.getElementById("Ho_ex_noc_dominical_text").innerText=` $ `+ parseInt(Ho_ex_noc_dominical_total2);
-        document.getElementById("valor_total_text").innerText=` $ `+ parseInt(valor_total);
+        document.getElementById("valor_total_text").innerText=` $ `+ parseInt(valor_total2);
+        document.getElementById("aux_Trans").innerText=` $ `+ parseInt(AuxTrans);
+        document.getElementById("salud_").innerText=` $ - `+ parseInt(salud);
+        document.getElementById("pension_").innerText=` $ - `+ parseInt(pension);
         
 }
 
    
                                        
+
 
 
 
